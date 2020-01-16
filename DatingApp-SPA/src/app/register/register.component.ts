875abb4d6122778c50cb47322df918b313e03cb3
Daniel Johansson <johansson.gd@gmail.com>
2020-01-16 19:25:40 +0100

    Klar med 4.48

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-register',
@@ -6,17 +8,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration sucessful');
+    }, error => {
+      console.log(error);
+    });
   }
 
   cancel() {
