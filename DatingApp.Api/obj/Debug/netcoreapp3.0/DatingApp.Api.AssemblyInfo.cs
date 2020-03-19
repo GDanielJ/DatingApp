641e2a6cfb433b5db56d4ba507f9818b885a7199
Daniel Johansson <johansson.gd@gmail.com>
2020-03-19 19:43:23 +0100

    Klar med 11.108

--- DatingApp.Api/obj/Debug/netcoreapp3.0/DatingApp.Api.AssemblyInfo.cs
+++ DatingApp.Api/obj/Debug/netcoreapp3.0/DatingApp.Api.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f050a499-28ad-4d4e-bb8b-eb679881521d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatingApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
