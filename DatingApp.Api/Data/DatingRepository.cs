73452cf09cd34a646e3cadd6fe3a895a14ebfe8d
Daniel Johansson <johansson.gd@gmail.com>
2020-04-22 20:01:33 +0200

    klar med 14.142

--- DatingApp.Api/Data/DatingRepository.cs
+++ DatingApp.Api/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
-﻿using DatingApp.Api.Models;
+﻿using DatingApp.Api.Helpers;
+using DatingApp.Api.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -43,11 +44,11 @@ namespace DatingApp.Api.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos);
 
-            return users;
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
