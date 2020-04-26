21460a13c4da888ed894478ab3811abfff7f4111
Daniel Johansson <johansson.gd@gmail.com>
2020-04-26 14:49:15 +0200

    klar med 14.146

--- DatingApp.Api/Helpers/UserParams.cs
+++ DatingApp.Api/Helpers/UserParams.cs
@@ -19,6 +19,7 @@ namespace DatingApp.Api.Helpers
 
 		public int UserId { get; set; }
 		public string Gender { get; set; }
-
+		public int MinAge { get; set; } = 18;
+		public int MaxAge { get; set; } = 99;
 	}
 }
