2beba78ea60d2db411190c006f47d782af01dfb1
Daniel Johansson <johansson.gd@gmail.com>
2020-05-10 18:01:15 +0200

    klar med 16.163

--- DatingApp.Api/Data/IDatingRepository.cs
+++ DatingApp.Api/Data/IDatingRepository.cs
@@ -18,7 +18,7 @@ namespace DatingApp.Api.Data
         Task<Photo> GetMainPhotoForUser(int userId);
         Task<Like> GetLike(int userId, int recipientId);
         Task<Message> GetMessage(int id);
-        Task<PagedList<Message>> GetMessageForUser();
+        Task<PagedList<Message>> GetMessageForUser(MessageParams messageParams);
         Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
     }
 }
