1f77ad69c9b709e4683042438ce69b4680afd51e
Daniel Johansson <johansson.gd@gmail.com>
2020-06-23 19:27:40 +0200

    klar med 20.204

--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -90,7 +90,7 @@ namespace DatingApp.Api
             services.AddCors();
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
-            services.AddScoped<IAuthRepository, AuthRepository>();
+            //services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddScoped<LogUserActivity>();
         }
